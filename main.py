58df80781bc2354130ff6053b2bc9b8e018f6b54
Попов Дмитрий Евгеньевич <DEvgPopov@sberbank.ru>
2020-06-15 00:44:00 +0300

    weather by location

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import asyncio
 
 from aiogram import Bot,Dispatcher, executor
-from config import BOT_TOKEN
+from config import BOT_TOKEN, OWM_TOKEN
+from pyowm import OWM  # OpenWeatherMap.org API
+from pyowm.utils.config import get_default_config
+
 
 loop = asyncio.get_event_loop()
+
 bot = Bot (BOT_TOKEN,parse_mode="HTML")
-dp= Dispatcher(bot, loop=loop)
+dp = Dispatcher(bot, loop=loop)
+
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
+owm = OWM(OWM_TOKEN, config_dict)  # initialize the Weather API
 
 if __name__=="__main__":
-    from handlers import dp,send_to_admin
+    from handlers import dp, send_to_admin
     executor.start_polling (dp, on_startup=send_to_admin)
