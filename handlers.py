0ef0c9d1a0d04ea48aa8e45391d60cec186dc198
Попов Дмитрий Евгеньевич <DEvgPopov@sberbank.ru>
2020-06-24 14:02:05 +0300

    added forecast

--- handlers.py
+++ handlers.py
@@ -47,10 +47,12 @@ async def get_location(call):
         obs = mgr.weather_at_coords(call.location.latitude, call.location.longitude)  # Create a weather observation
         forc = mgr.forecast_at_coords(call.location.latitude, call.location.longitude, '3h')
         w = obs.weather
-        f = forc.forecast
+        f = forc.forecast.weathers[0]
         l = obs.location
+        # location
         placename = str(l.name)
         country = str(l.country)
+        # weather
         d_stat = str(w.detailed_status)
         ref_time = str(w.reference_time('date'))
         wind = str(w.wnd.get('speed'))
@@ -59,15 +61,25 @@ async def get_location(call):
         pressure = str(w.pressure.get('press'))
         temperature = str(w.temperature('celsius').get('temp'))
         t_feels = str(w.temperature('celsius').get('feels_like'))
+        weather_icon_name = w.weather_icon_name
         rain = str(w.rain.get('_len_'))
         snow = str(w.snow.get('_len_'))
-        forcast_d_stat = str(f.weathers.get(index))
+        # forecast
+        f_d_stat = str(f.detailed_status)
+        f_temperature = str(f.temperature('celsius').get('temp'))
+        f_t_feels = str(f.temperature('celsius').get('feels_like'))
+        f_wind = str(f.wnd.get('speed'))
+        f_weather_icon_name = str(f.weather_icon_name)
+
+        print(f_d_stat, f_temperature, f_t_feels, f_weather_icon_name, weather_icon_name)
         print(l.name, l.country, wind, pressure, d_stat, temperature)
 
         await message1.answer(
             'Сейчас (' + ref_time + ') \nпогода в ' + placename + ',' + country + ': \n' + d_stat + ', температура: ' +
-            temperature + '°C,\n' + 'чувствуется как: '+t_feels + '°C'+', \nветер: ' + wind + 'м/с' + ', \nдавление: '
-            + pressure + 'мм.рт.ст.\nРассвет: ' + srise_time + '.\nЗакат: ' + sset_time + '\nБлижайшие 3 часа: ' +
-            forcast_d_stat)
+            temperature + '°C,\n' + 'чувствуется как: ' + t_feels + '°C' + ', \nветер: ' + wind + 'м/с' +
+            ', \nдавление: '
+            + pressure + 'мм.рт.ст.\nРассвет: ' + srise_time + '.\nЗакат: ' + sset_time +
+            '\n\nБлижайшие 3 часа: \n' + f_d_stat + ',\n температура: ' + f_temperature + '°C,' + '\nчувствуется как: '
+            + f_t_feels + '°C' + ', \nветер: ' + f_wind + 'м/с')
     else:
         await message1.reply('Это не геолокация!')
